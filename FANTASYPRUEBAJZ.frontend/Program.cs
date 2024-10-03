9fbbb0759d9c765c5838d13c20e17eb17863b022
Gerard0000 <gerardramos2011@gmail.com>
2024-10-02 18:15:55 -0600

    Created repository since frontend

--- FANTASYPRUEBAJZ.frontend/Program.cs
+++ FANTASYPRUEBAJZ.frontend/Program.cs
@@ -1,4 +1,5 @@
 using FANTASYPRUEBAJZ.frontend;
+using FANTASYPRUEBAJZ.frontend.Repositories;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -6,6 +7,11 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+//Conexión del backend con el frontend
+//builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("https://localhost:7111") });
+
+//Irepository
+builder.Services.AddScoped<IRepository, Repository>();
 
 await builder.Build().RunAsync();
