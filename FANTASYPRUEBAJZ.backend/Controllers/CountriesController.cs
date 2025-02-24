4817506305896cbe7410593ca8a496df1f214fd7
Gerard0000 <gerardramos2011@gmail.com>
2025-02-23 20:27:45 -0600

    Add Countries Api Controller

---
+++ FANTASYPRUEBAJZ.backend/Controllers/CountriesController.cs
@@ -0,0 +1,75 @@
+﻿using FANTASYPRUEBAJZ.backend.Data;
+using FANTASYPRUEBAJZ.shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FANTASYPRUEBAJZ.backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            return Ok(await _context.Countries.ToListAsync());
+        }
+
+        //PASANDO EL PARAMETRO POR RUTA
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            return Ok(country);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            var currentCountry = await _context.Countries.FindAsync(country.Id);
+            if (currentCountry == null)
+            {
+                return NotFound();
+            }
+            currentCountry.Name = country.Name;
+            _context.Update(currentCountry);
+            await _context.SaveChangesAsync();
+            //NOCONTENT ES PARA QUE NO ME TRANSPORTE EL OBJETO EDITADO
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            //NOCONTENT ES PARA QUE NO ME TRANSPORTE EL OBJETO EDITADO
+            return NoContent();
+        }
+    }
+}
